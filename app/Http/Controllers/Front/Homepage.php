a4923da57d7027d7a1e4dc0efbf5f90eabdd3123
Elnur Məmmədov <baytbybayt@gmail.com>
2020-09-21 22:54:54 +0400

    to make the code more cleaner, function documentation added

--- app/Http/Controllers/Front/Homepage.php
+++ app/Http/Controllers/Front/Homepage.php
@@ -12,23 +12,25 @@ use App\Models\Category;
 
 class Homepage extends Controller
 {
+    // index function returns list of articles with slider
     public function index(){
-        $data['articles']=Article::orderBy('created_at','DESC')->get();  
+        $data['articles']=Article::orderBy('created_at','DESC')->get();
         $data['categories']=Category::inRandomOrder()->get();
         return view('front.homepage',$data);
     }
+
+    // single
     public function single($category,$slug){
-        $category = Category::whereSlug($category)->first() ?? abort(403,'Tapilmadi'); 
+        $category = Category::whereSlug($category)->first() ?? abort(403,'Tapilmadi');
         $article = Article::whereSlug($slug)->whereCategoryId($category->id)->first() ?? abort(403,'Tapilmadi');
         $article->increment('hit');
         $data['article']= $article;
-        $data['categories']=Category::inRandomOrder()->get();
+        $data['categoroes']=Category::inRandomOrder()->get();
         return view('front.single',$data);
     }
 
     public function category($slug){
-        $category = Category::whereSlug($slug)->first() ?? abort(403,'Tapilmadi'); 
-        $data['category'] = $category;
+        $category = Category::whereSlug($slug)->first() ?? abort(403,'Tapilmadi');
         $data['articles'] = Article::where('category_id',$category->id)->orderBy('created_at','DESC')->get();
         $data['categories']=Category::inRandomOrder()->get();
         return view ('front.category', $data);
